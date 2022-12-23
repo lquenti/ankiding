684154b76a6f502cceda640fcb16410323999d6b
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2022-12-23 10:33:46 +0100

    initial CLI parsing

--- ankiding/src/main.rs
+++ ankiding/src/main.rs
@@ -1,3 +1,16 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// The path to the markdown files
+    path: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    println!("name: {:?}", cli.path);
 }
