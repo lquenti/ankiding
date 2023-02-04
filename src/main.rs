f5715defe1964defccf01cbd81d012b90cbf3563
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2023-02-04 13:01:59 +0100

    WIP: Img extractor before refactor

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use crate::parser::Card;
 
-//use std::collections::HashMap;
+use std::collections::HashMap;
 use std::path::PathBuf;
 
 use anyhow::Result;
 use clap::Parser;
+use tempfile::TempDir;
 
 mod anki;
 mod io;
@@ -20,9 +21,13 @@ struct Cli {
     output: Option<PathBuf>,
 }
 
-
 fn main() -> Result<()> {
-    let _markdowns = io::get_all_files(Cli::parse().path)?.into_iter()
+    let mut replacements = HashMap::new();
+    let temp_dir = TempDir::new()?;
+    let external_files = HashMap::new();
+
+    let _markdowns = io::get_all_files(Cli::parse().path)?
+        .into_iter()
         // Read files
         .map(|filename| {
             let markdowns = io::read_file_to_string(&filename).unwrap();
@@ -31,14 +36,25 @@ fn main() -> Result<()> {
         // Convert to cards
         .map(|(filename, markdowns)| (filename, Card::from_markdown(&markdowns)))
         // Convert to html
-        .map(|(filename, cards)| (filename, cards.into_iter().map(Card::to_html).collect::<Vec<Card>>()))
+        .map(|(filename, cards)| {
+            (
+                filename,
+                cards.into_iter().map(Card::to_html).collect::<Vec<Card>>(),
+            )
+        })
+        // Extract img tags
+        .map(|(filename, cards)| {
+            //external_files.extend(
+            //    Card::mass_apply_to_vec(cards.clone(), parser::extract_img_paths_from_html)
+            //);
+            (filename, cards)
+        })
         // Convert to Ankideck
         .map(|(filename, cards)| {
             let deck = anki::from_cards(&filename, &cards);
-            (filename, deck)}
-            )
+            (filename, deck)
+        })
         .collect::<Vec<(PathBuf, genanki_rs::Deck)>>();
-
 
     Ok(())
 }
