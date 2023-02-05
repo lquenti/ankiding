e4bf892f95f9540f15dada57ab85dd8b6b672436
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2023-02-05 12:40:03 +0100

    back to only what works

--- src/parser.rs
+++ src/parser.rs
@@ -4,8 +4,10 @@ use regex::Regex;
 
 lazy_static! {
     static ref COMRAK_OPTIONS: ComrakOptions = {
-        // TODO SET OPTIONS
-        ComrakOptions::default()
+        let mut options = ComrakOptions::default();
+        options.render.unsafe_ = true;
+        options.extension.table = true;
+        options
     };
 }
 
@@ -13,7 +15,9 @@ lazy_static! {
     static ref MARKDOWN_CARDS_RE: Regex =
         Regex::new(r"---\n\s*Q:(?P<question>(.|\n|\r)*?)\n---\n\s*A:(?P<answer>(.|\n|\r)*?)\n---")
             .unwrap();
-    static ref IMG_RE: Regex = Regex::new(r#"<img src=["'](?P<src>.*?)["'].*?/>"#).unwrap();
+    //static ref IMG_RE: Regex = Regex::new(r#"<img[^>]*?src=["'](?P<src>.*?)["'].*?/?>"#).unwrap();
+    //static ref IMG_RE: Regex = Regex::new(r#"<(img|source|video)[^>]*?src=["'](?P<src>.*?)["'][^>]*?/?>"#).unwrap();
+    static ref IMG_RE: Regex = Regex::new(r#"<video[^>]*?src=["'](?P<src>.*?)["'][^>]*?/?>"#).unwrap();
 }
 
 #[derive(Debug, Clone)]
@@ -39,33 +43,4 @@ impl Card {
         let back = markdown_to_html(&self.back, &COMRAK_OPTIONS);
         Card { front, back }
     }
-
-    pub fn map(self, f: impl Fn(String) -> String) -> Self {
-        let front = f(self.front);
-        let back = f(self.back);
-        Card { front, back }
-    }
-
-    pub fn apply<A>(self, f: impl Fn(&str) -> A) -> (A, A) {
-        (f(&self.front), f(&self.back))
-    }
-
-    // TODO Rename me
-    pub fn mass_apply_to_vec<A>(cards: Vec<Self>, f: impl Fn(&str) -> Vec<A>) -> Vec<A> {
-        cards
-            .into_iter()
-            .map(|card| card.apply(&f))
-            .flat_map(|(xs, ys)| xs.into_iter().chain(ys.into_iter()).collect::<Vec<A>>())
-            .collect::<Vec<A>>()
-    }
-}
-
-// TODO: MOVE ME
-pub fn extract_img_paths_from_html(html: &str) -> Vec<String> {
-    let mut paths = Vec::new();
-    for cap in IMG_RE.captures_iter(html) {
-        let path = cap.name("src").unwrap().as_str().trim().to_string();
-        paths.push(path);
-    }
-    paths
 }
